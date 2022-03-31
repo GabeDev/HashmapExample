b26d75fbcf1bc12df8026c37b824189e92036e29
GabeDev <gabriel.d.albu@gmail.com>
2022-03-31 09:23:30 -0400

    creating a basic map example

--- HashmapExample.cpp
+++ HashmapExample.cpp
@@ -1,20 +1,18 @@
-// HashmapExample.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <map>
+#include <string>
 
+using namespace std;
 int main()
 {
     std::cout << "Hello World!\n";
+    map<string, string> mydictionairy;
+    mydictionairy.insert(pair<string,string>("apple", "derApfel"));
+    mydictionairy.insert(pair<string, string>("orange", "die orange"));
+    mydictionairy.insert(pair<string, string>("banana", "die banana"));
+
+    for  (auto pair : mydictionairy)
+    {
+        cout << pair.first << "-" << pair.second << endl;
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
